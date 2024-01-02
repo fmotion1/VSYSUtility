d3e2301028c95c7d1b05d80386e1a5018c322587
fmotion1 <futuremotionrecovery@gmail.com>
2024-01-02 13:14:37 -0600

    Update file paths and remove unused files

--- src/VSYSSystemStructs/obj/Debug/net6.0/VSYSSystemStructs.AssemblyInfo.cs
+++ src/VSYSSystemStructs/obj/Debug/net6.0/VSYSSystemStructs.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VSYSSystemStructs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b38f7c3e54d2b29b97f9d64456db2ed437f80470")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("VSYSSystemStructs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VSYSSystemStructs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
